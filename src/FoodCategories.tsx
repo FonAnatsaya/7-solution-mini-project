72362f77c8de0f9ed62ac1faa19029c0f4fea001
fon <anatsaya.lee@gmail.com>
2024-05-14 13:46:50 +0700

    first commit

---
+++ src/FoodCategories.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FoodCategories = () => {
+  return <div>FoodCategories</div>;
+};
+
+export default FoodCategories;
---
+++ src/FoodLists.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FoodLists = () => {
+  return <div>FoodLists</div>;
+};
+
+export default FoodLists;
